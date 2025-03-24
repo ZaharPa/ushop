fe2a1a5f88fcfac00656c8f9568b516faab1ba4b
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-03-24 12:03:23 +0000

    Add initial project structure with basic configuration

---
+++ resources/js/Pages/Auth/Login.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div>
+            <h1 className="text-amber-600">Login</h1>
+        </div>
+    );
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Ushop</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @vite('resources/css/app.css')
+    @routes
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
