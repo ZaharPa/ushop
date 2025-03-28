0f281ada0046cbcb5cb71e01268d27885ca74e9c
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-03-28 11:20:39 +0000

    Implement login functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function create()
     {
         return inertia('Auth/Login');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'remember' => 'nullable|boolean'
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->has('remember'))) {
+            throw ValidationException::withMessages([
+                'email' => 'Authentification failed'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended('/');
+    }
+
+
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,12 +1,22 @@
-import { Link } from "@inertiajs/react";
+import { Link, useForm } from "@inertiajs/react";
 
 export default function Login() {
+    const { data, setData, post, processing, errors } = useForm({
+        email: "",
+        password: "",
+        remember: false,
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        post(route("login.store"));
+    };
+
     return (
         <div className="w-3/4 lg:w-1/2 mx-auto mt-10 p-4 bg-sky-100 shadow-lg rounded-lg text-lg">
             <h1 className="text-2xl text-center p-2 mb-6">Login</h1>
             <form
-                method="POST"
-                action="#"
+                onSubmit={handleSubmit}
                 className="flex flex-col space-y-4 text-emerald-900"
             >
                 <div className="flex gap-4 items-center">
@@ -14,25 +24,49 @@ export default function Login() {
                     <input
                         type="email"
                         name="email"
-                        required
-                        className="flex-1 px-2 border border-emerald-900 rounded-lg bg-white"
-                    />
-                </div>
-                <div className="flex gap-4 items-center">
-                    <span className="w-20">Password</span>
-                    <input
-                        type="password"
-                        name="password"
+                        value={data.email}
+                        onChange={(e) => setData("email", e.target.value)}
                         required
                         className="flex-1 px-2 border border-emerald-900 rounded-lg bg-white"
                     />
                 </div>
 
+                <div className="flex gap-4 items-center">
+                    <span className="w-20">Password</span>
+                    <input
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        onChange={(e) => setData("password", e.target.value)}
+                        required
+                        className="flex-1 px-2 border border-emerald-900 rounded-lg bg-white"
+                    />
+                </div>
+
+                <div>
+                    <span className="mt-2 ">Remember me</span>
+                    <input
+                        type="checkbox"
+                        name="remember"
+                        checked={data.remember}
+                        onChange={(e) => setData("remember", e.target.checked)}
+                        className="w-10 h-4"
+                    />
+                </div>
+
+                <div className="text-center">
+                    {errors.email && (
+                        <span className="text-red-500">{errors.email}</span>
+                    )}
+                </div>
+
                 <button
                     type="submit"
-                    className="border border-gray-400 rounded-lg bg-emerald-300 text-emerald-800 font-semibold w-fit self-center mt-2 px-3 py-1 cursor-pointer hover:bg-sky-50 hover:text-emerald-600"
+                    disabled={processing}
+                    className="border border-gray-400 rounded-lg bg-emerald-300 text-emerald-800 font-semibold w-fit self-center px-3 py-1 cursor-pointer hover:bg-sky-50 hover:text-emerald-600
+                    disabled:cursor-not-allowed disabled:bg-emerald-600 disabled:text-emerald-100"
                 >
-                    Sign in
+                    {processing ? "Logging in..." : "Sign in"}
                 </button>
                 <div className="text-center mt-2">
                     Don't have an account? Click
