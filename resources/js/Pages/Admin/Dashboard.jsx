bc0346ac56abe4e088fb35118dbf471d7f11917e
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-04-04 12:38:15 +0100

    Start admin panel

---
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        <>
+            <div>Left slider with navigation</div>
+            <div>Graphs</div>
+            <div>Some top products</div>
+        </>
+    );
+}
