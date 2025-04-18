ad518128136e3a2238c0fd261a74069ad591857f
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-04-18 12:02:54 +0100

    Implement Filter funciton for products

--- resources/js/Pages/Admin/Product/Index.jsx
+++ resources/js/Pages/Admin/Product/Index.jsx
@@ -1,22 +1,65 @@
 import Pagination from "@/Components/Pagination";
 import AdminLayout from "@/Layouts/AdminLayout";
-import { usePage } from "@inertiajs/react";
+import { router, useForm, usePage } from "@inertiajs/react";
 
 export default function Index() {
     const { products, categories, filters } = usePage().props;
+
+    const { data, setData, get, processing, reset } = useForm({
+        name: filters.name || "",
+        priceFrom: filters.priceFrom || "",
+        priceTo: filters.priceTo || "",
+        category: filters.category || "",
+    });
+
+    const handleFilter = (e) => {
+        e.preventDefault();
+        get(route("admin.product.index"), {
+            onSuccess: () => {
+                reset();
+            },
+        });
+    };
+
+    const handleReset = () => {
+        setData({
+            name: "",
+            priceFrom: "",
+            priceTo: "",
+            category: "",
+        });
+
+        reset();
+
+        router.visit(route("admin.product.index"), {
+            replace: true,
+            preserveState: false,
+        });
+    };
 
     return (
         <AdminLayout>
             <h2 className="h2-center">Product List</h2>
             <ul className="w-full">
                 <li>
-                    <form className="flex flex-wrap w-full justify-center gap-2 mb-3">
+                    <form
+                        onSubmit={handleFilter}
+                        className="flex flex-wrap w-full justify-center gap-2 mb-3"
+                    >
                         <input
                             type="text"
                             placeholder="Name product"
+                            value={data.name}
+                            onChange={(e) => setData("name", e.target.value)}
                             className="input-admin w-full"
                         />
-                        <select className="input-admin">
+                        <select
+                            className="input-admin"
+                            defaultValue={data.category}
+                            onChange={(e) =>
+                                setData("category", e.target.value)
+                            }
+                        >
                             <option value={""}>Category</option>
                             {categories.map((category) => (
                                 <option key={category.id} value={category.id}>
@@ -27,18 +70,32 @@ export default function Index() {
                         <input
                             type="text"
                             placeholder="Price From"
+                            value={data.priceFrom}
+                            onChange={(e) =>
+                                setData("priceFrom", e.target.value)
+                            }
                             className="input-admin"
                         />
                         <input
                             type="text"
                             placeholder="Price To"
+                            value={data.priceTo}
+                            onChange={(e) => setData("priceTo", e.target.value)}
                             className="input-admin"
                         />
                         <div className="flex gap-3">
-                            <button type="submit" className="btn-admin">
-                                Filter
+                            <button
+                                type="submit"
+                                disabled={processing}
+                                className="btn-admin"
+                            >
+                                {processing ? "Applying..." : "Filter"}
                             </button>
-                            <button type="reset" className="btn-reset">
+                            <button
+                                type="reset"
+                                onClick={handleReset}
+                                className="btn-reset"
+                            >
                                 Reset
                             </button>
                         </div>
@@ -53,7 +110,10 @@ export default function Index() {
                     <div></div>
                 </li>
                 {products.data.map((product) => (
-                    <li className="grid grid-cols-9 gap-2 mt-1 text-center">
+                    <li
+                        key={product.id}
+                        className="grid grid-cols-9 gap-2 mt-1 text-center"
+                    >
                         <div>{product.id}</div>
                         <div className="col-span-2">photo</div>
                         <div className="col-span-2">{product.name}</div>
