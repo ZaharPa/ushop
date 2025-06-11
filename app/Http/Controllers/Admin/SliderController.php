ad073b7002580495a3c6685ce59526cd5560a039
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-06-11 12:06:24 +0100

    Enchance slider-admin UI and contoller

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SliderController extends Controller
 {
@@ -17,16 +18,60 @@ class SliderController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string|max:255',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'order' => 'nullable|integer|min:0',
+            'link' => 'required|url',
+        ]);
+
+        $imagePath = $request->file('image')->store('slider', 'public');
+
+        Slider::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $imagePath,
+            'order' => $request->order,
+            'link' => $request->link
+        ]);
+
+        return back()->with('success', 'Slide created successfully!');
     }
 
     public function update(Request $request, Slider $slider)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'order' => 'nullable|integer|min:0',
+            'link' => 'required|url',
+        ]);
+
+        if ($request->hasFile('image')) {
+            Storage::disk('public')->delete($slider->image);
+        }
+
+        $slider->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $request->hasFile('image')
+                ? $request->file('image')->store('slider', 'public')
+                : $slider->image,
+            'order' => $request->order,
+            'link' => $request->link,
+        ]);
+
+        return back()->with('success', 'Slide updated successfully!');
     }
 
     public function destroy(Slider $slider)
     {
-        //
+        Storage::disk('public')->delete($slider->image);
+
+        $slider->delete();
+
+        return back()->with('success', 'Slide deleted successfully!');
     }
 }
--- resources/js/Pages/Admin/Slider.jsx
+++ resources/js/Pages/Admin/Slider.jsx
@@ -1,19 +1,20 @@
+import ConfrimModal from "@/Components/ConfrimModal";
 import Pagination from "@/Components/Pagination";
 import AdminLayout from "@/Layouts/AdminLayout";
-import { useForm, usePage } from "@inertiajs/react";
+import { router, useForm, usePage } from "@inertiajs/react";
 import { useState } from "react";
 
 export default function Slider() {
     const { slides, errors } = usePage().props;
 
     const [showForm, setShowForm] = useState(false);
+    const [showConfirm, setShowConfirm] = useState(false);
     const [chosenSlide, setChosenSlide] = useState(null);
 
     const {
         data,
         setData,
         post,
-        put,
         delete: destroy,
         processing,
         reset,
@@ -43,6 +44,55 @@ export default function Slider() {
             active: slide.active,
         });
         setShowForm(true);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (chosenSlide) {
+            const formData = new FormData();
+            formData.append("title", data.title);
+            formData.append("description", data.description);
+
+            if (data.image) {
+                formData.append("image", data.image);
+            }
+
+            formData.append("link", data.link);
+            formData.append("position", data.position);
+            formData.append("active", data.active);
+
+            router.put(route("admin.slider.update", chosenSlide.id), formData, {
+                forceFormData: true,
+                onSuccess: () => {
+                    setShowForm(false);
+                    setChosenSlide(null);
+                    reset();
+                },
+            });
+        } else {
+            post(route("admin.slider.store"), {
+                forceFormData: true,
+                onSuccess: () => {
+                    setShowForm(false);
+                    reset();
+                },
+            });
+        }
+    };
+
+    const handleDelete = (slide) => {
+        setChosenSlide(slide);
+        setShowConfirm(true);
+    };
+
+    const confirmDelete = () => {
+        destroy(route("admin.slider.destroy", chosenSlide.id), {
+            onSuccess: () => {
+                setShowConfirm(false);
+                setChosenSlide(null);
+                reset();
+            },
+        });
     };
 
     return (
@@ -102,6 +152,163 @@ export default function Slider() {
             <div className="flex justify-center">
                 <Pagination links={slides.links} />
             </div>
+
+            {showForm && (
+                <div className="dotted-form">
+                    <form
+                        onSubmit={handleSubmit}
+                        encType="multipart/form-data"
+                        className="text-center"
+                    >
+                        <h2 className="h2-center">
+                            {chosenSlide ? "Edit Slide" : "Add new Slide"}
+                        </h2>
+
+                        {chosenSlide && <div>Slide ID - {chosenSlide.id}</div>}
+
+                        <div className="my-3">
+                            <span>Title - </span>
+                            <input
+                                type="text"
+                                value={data.title}
+                                onChange={(e) =>
+                                    setData("title", e.target.value)
+                                }
+                                className="input-admin"
+                            />
+                        </div>
+                        {errors.title && (
+                            <div className="text-red-500 my-1">
+                                {errors.title}
+                            </div>
+                        )}
+
+                        <div>
+                            <span>Description - </span>
+                            <textarea
+                                value={data.description}
+                                size={data.description.length}
+                                onChange={(e) =>
+                                    setData("description", e.target.value)
+                                }
+                                className="input-admin"
+                            ></textarea>
+                        </div>
+                        {errors.description && (
+                            <div className="text-red-500 my-1">
+                                {errors.description}
+                            </div>
+                        )}
+
+                        <div className="my-3">
+                            <span>Photo - </span>
+                            <input
+                                type="file"
+                                onChange={(e) =>
+                                    setData("image", e.target.files[0])
+                                }
+                                className="input-admin"
+                            />
+                        </div>
+                        {errors.image && (
+                            <div className="text-red-500 my-1">
+                                {errors.image}
+                            </div>
+                        )}
+
+                        <div>
+                            <span>Link - </span>
+                            <input
+                                type="text"
+                                value={data.link}
+                                size={data.link.length}
+                                onChange={(e) =>
+                                    setData("link", e.target.value)
+                                }
+                                className="input-admin"
+                            />
+                        </div>
+                        {errors.link && (
+                            <div className="text-red-500 my-1">
+                                {errors.link}
+                            </div>
+                        )}
+
+                        <div className="my-3">
+                            <span>Position - </span>
+                            <input
+                                type="number"
+                                value={data.position}
+                                onChange={(e) =>
+                                    setData("position", e.target.value)
+                                }
+                                className="input-admin"
+                            />
+                        </div>
+                        {errors.position && (
+                            <div className="text-red-500 my-1">
+                                {errors.position}
+                            </div>
+                        )}
+
+                        <div>
+                            <span>Active - </span>
+                            <input
+                                type="checkbox"
+                                value={data.active}
+                                checked={data.active}
+                                onChange={(e) =>
+                                    setData("active", e.target.checked)
+                                }
+                            />
+                        </div>
+                        {errors.active && (
+                            <div className="text-red-500 my-1">
+                                {errors.active}
+                            </div>
+                        )}
+
+                        <div className="flex justify-center gap-4 mt-4">
+                            <button
+                                type="submit"
+                                disabled={processing}
+                                className="btn-primary"
+                            >
+                                {chosenSlide ? "Update" : "Create"}
+                            </button>
+                            {chosenSlide && (
+                                <button
+                                    type="button"
+                                    onClick={() => handleDelete(chosenSlide)}
+                                    className="btn-delete"
+                                >
+                                    Delete
+                                </button>
+                            )}
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    setChosenSlide(null);
+                                    setShowForm(false);
+                                    reset();
+                                }}
+                                className="btn-reset"
+                            >
+                                Reset
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            )}
+
+            <ConfrimModal
+                show={showConfirm}
+                onConfrim={confirmDelete}
+                onCancel={() => {
+                    setShowConfirm(false);
+                }}
+                message="Are you sure want to delete this slide?"
+            />
         </AdminLayout>
     );
 }
