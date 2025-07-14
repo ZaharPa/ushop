ca627234560ebe56cf3874f98eb65747b17bd1e3
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-07-14 11:23:46 +0100

    Create checkout store function

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CheckoutController extends Controller
 {
@@ -24,6 +27,43 @@ class CheckoutController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:150',
+            'phone' => "required|string|max:15",
+            'address' => "required|string|max:150",
+            'address2' => "nullable|string|max:150",
+            'email' => "nullable|email",
+        ]);
+
+        $cart = session('cart', []);
+        $items = Item::find(array_keys($cart));
+
+        $total_price = $items->sum(function ($item) use ($cart) {
+            return $item->price * $cart[$item->id];
+        });
+
+        DB::transaction(function () use ($request, $cart, $items, $total_price, &$order) {
+            $order = Order::create([
+                'user_id' => auth()->user()?->id,
+                'name' => $request->name,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'address2' => $request->address2,
+                'email' => $request->email,
+                'total_price' => $total_price,
+            ]);
+
+            foreach ($items as $item) {
+                OrderItem::create([
+                    'order_id' => $order->id,
+                    'item_id' => $item->id,
+                    'price' => $item->price,
+                    'quantity' => $cart[$item->id],
+                    'subtotal' => $item->price * $cart[$item->id],
+                ]);
+            }
+        });
+
+        return inertia('Cart/Payment', ['order' => $order]);
     }
 }
