f621506f3081aa50fd90fd8d52fff3dfa95e6fef
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-08-04 11:30:10 +0100

    update order UI admin panel

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -13,8 +13,14 @@ class OrderController extends Controller
     public function index()
     {
         return inertia('Admin/Order/Index', [
-            'orders' => Order::orderBy('created_at', 'desc')
-                ->paginate(10)
+            'ordersByStatus' => [
+                'pending' => Order::where('status', 'pending')->orderBy('created_at', 'desc')->paginate(10, ['*'], 'pending_page'),
+                'confirmed' => Order::where('status', 'confirmed')->orderBy('created_at', 'desc')->paginate(10, ['*'], 'confirmed_page'),
+                'paid' => Order::where('status', 'paid')->orderBy('created_at', 'desc')->paginate(10, ['*'], 'paid_page'),
+                'shipped' => Order::where('status', 'shipped')->orderBy('created_at', 'desc')->paginate(10, ['*'], 'shipped_page'),
+                'delivered' => Order::where('status', 'delivered')->orderBy('created_at', 'desc')->paginate(10, ['*'], 'delivered_page'),
+                'cancelled' => Order::where('status', 'cancelled')->orderBy('created_at', 'desc')->paginate(10, ['*'], 'cancelled_page'),
+            ]
         ]);
     }
 
--- resources/js/Pages/Admin/Order/Index.jsx
+++ resources/js/Pages/Admin/Order/Index.jsx
@@ -3,42 +3,47 @@ import AdminLayout from "@/Layouts/AdminLayout";
 import { Link, usePage } from "@inertiajs/react";
 
 export default function Index() {
-    const { orders } = usePage().props;
+    const { ordersByStatus } = usePage().props;
 
     return (
         <AdminLayout>
             <h2 className="h2-center">Orders List</h2>
-            <ul className="w-full">
-                <li className="grid grid-cols-5 gap-4 mb-2 text-center font-medium text-lg">
-                    <div>ID</div>
-                    <div>Customer</div>
-                    <div>Total</div>
-                    <div>Status</div>
-                    <div></div>
-                </li>
-                {orders.data.map((order) => (
-                    <li
-                        key={order.id}
-                        className="grid grid-cols-5 gap-4 mb-2 text-center"
-                    >
-                        <div>{order.id}</div>
-                        <div>{order.name}</div>
-                        <div>{order.total_price}</div>
-                        <div>{order.status}</div>
-                        <Link
-                            href={route("admin.order.show", order.id)}
-                            type="button"
-                            className="btn-admin"
-                        >
-                            View
-                        </Link>
-                    </li>
-                ))}
+            {Object.entries(ordersByStatus).map(([status, orders]) => (
+                <div key={status} className="mb-12">
+                    <h3 className="text-center text-xl font-bold mb-2 capitalize">
+                        {status}
+                    </h3>
+                    <ul className="w-full">
+                        <li className="grid grid-cols-4 gap-4 mb-2 text-center font-medium text-lg">
+                            <div>ID</div>
+                            <div>Customer</div>
+                            <div>Total</div>
+                            <div></div>
+                        </li>
+                        {orders.data.map((order) => (
+                            <li
+                                key={order.id}
+                                className="grid grid-cols-4 gap-4 mb-2 text-center"
+                            >
+                                <div>{order.id}</div>
+                                <div>{order.name}</div>
+                                <div>{order.total_price}</div>
+                                <Link
+                                    href={route("admin.order.show", order.id)}
+                                    type="button"
+                                    className="btn-admin"
+                                >
+                                    View
+                                </Link>
+                            </li>
+                        ))}
 
-                <li className="mt-6 mx-auto w-fit">
-                    <Pagination links={orders.links} />
-                </li>
-            </ul>
+                        <li className="mt-6 mx-auto w-fit">
+                            <Pagination links={orders.links} />
+                        </li>
+                    </ul>
+                </div>
+            ))}
         </AdminLayout>
     );
 }
